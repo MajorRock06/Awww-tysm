468632aae1e753523ddf54c34bf321a3604b3afd
MajorRock06 <mikk476r@mail.hansenberg.dk>
2024-01-16 12:56:47 +0100

    begge

--- I_Have_Autism/Program.cs
+++ I_Have_Autism/Program.cs
@@ -6,3 +6,15 @@ Array.Reverse(stringarray);
 string revWord = new string(stringarray);
 
 Console.WriteLine("the reversed Word is: {0}", revWord);
+
+
+
+
+int Age = 0; 
+ 
+Console.WriteLine("Hello, Write your age:\n"); 
+Age = Convert.ToInt32(Console.ReadLine()); 
+ 
+int kAge = Age + 1; 
+ 
+Console.WriteLine("you're age next year is: " + kAge);
