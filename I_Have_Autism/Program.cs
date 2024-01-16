73639aede9519b5d0165bbf923d7e109311952bf
MajorRock06 <mikk476r@mail.hansenberg.dk>
2024-01-16 10:38:47 +0100

    Age++
    Age next year

--- I_Have_Autism/Program.cs
+++ I_Have_Autism/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Age = 0;
+
+Console.WriteLine("Hello, Write your age:\n");
+Age = Convert.ToInt32(Console.ReadLine());
+
+int kAge = Age + 1;
+
+Console.WriteLine("you're age next year is: " + kAge);
