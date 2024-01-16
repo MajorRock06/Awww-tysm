f42ab748509747829e6627a63f54c7a637a34a2d
MajorRock06 <mikk476r@mail.hansenberg.dk>
2024-01-16 14:49:30 +0100

    yes

--- I_Have_Autism/Program.cs
+++ I_Have_Autism/Program.cs
@@ -1,4 +1,18 @@
-﻿Console.WriteLine("Hello, write af word you want in reverse:\n");
+﻿Console.WriteLine("Hello, Write your age:\n"); 
+int Age = Convert.ToInt32(Console.ReadLine()); 
+ 
+Console.WriteLine("you're age next year is: {0}", (Age + 1));
+
+
+
+
+
+
+
+
+
+/*
+Console.WriteLine("Hello, write af word you want in reverse:\n");
 string Word = Console.ReadLine()!;
 
 char[] stringarray = Word.ToCharArray();
@@ -6,15 +20,4 @@ Array.Reverse(stringarray);
 string revWord = new string(stringarray);
 
 Console.WriteLine("the reversed Word is: {0}", revWord);
-
-
-
-
-int Age = 0; 
- 
-Console.WriteLine("Hello, Write your age:\n"); 
-Age = Convert.ToInt32(Console.ReadLine()); 
- 
-int kAge = Age + 1; 
- 
-Console.WriteLine("you're age next year is: " + kAge);
+*/
