3508359f0ff6c228a24604f7b6f0407d2c28a38c
MajorRock06 <mikk476r@mail.hansenberg.dk>
2024-03-04 22:10:39 +0100

    fremlæggelse
    s

--- I_Have_Autism/Program.cs
+++ I_Have_Autism/Program.cs
@@ -1,1 +1,22 @@
 ﻿
+int[] mitArray1 = new int[5];
+
+int[] mitArray2 = new int[] {1, 2, 3, 4, 5};
+
+
+
+
+
+
+
+// Deklaration og initialisering af et array af integers
+int[] minArray = { 5, 10, 15, 20, 25 };
+
+// Udskriv hvert element i arrayet
+Console.WriteLine("Elementer i arrayet:");
+for (int i = 0; i < minArray.Length; i++)
+{
+    Console.WriteLine($"Element {i + 1}: {minArray[i]}");
+}
+
+
